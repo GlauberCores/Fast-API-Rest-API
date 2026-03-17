4d5d80c05343a2fdf5d40bf7c6756cda6b40788b
GlauberCores <glaubercores@gmail.com>
2026-03-17 20:23:53 -0300

    refactor: reorganizar estrutura da API e dependências

--- dependencias.py
+++ dependencias.py
@@ -4,13 +4,8 @@ from models import *
 from dotenv import load_dotenv
 from jose import jwt, JWTError
 from fastapi import Depends, HTTPException
+from security import SECRET_KEY, ALGORITHM, bcrypt_context,OAuth2PasswordBearer
 import os
-
-
-load_dotenv()
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 
 def pegar_sessao():
@@ -21,10 +16,10 @@ def pegar_sessao():
     finally:
         session.close()
 
-def verificar_token(token, session: Session = Depends(pegar_sessao)):
+def verificar_token(token:str=Depends(OAuth2PasswordBearer), session: Session = Depends(pegar_sessao)):
     try:
         dic_info = jwt.decode(token, SECRET_KEY, ALGORITHM)
-        id_usuario = dic_info.get("sub")
+        id_usuario = int(dic_info.get("sub"))
 
         if not id_usuario:
             raise HTTPException(status_code=401, detail="Token inválido")
