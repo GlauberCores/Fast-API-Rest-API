9142adbf8e5616312063f5c30a942ed74f0c30f2
GlauberCores <glaubercores@gmail.com>
2026-03-17 19:15:03 -0300

    fix: corrigir autenticação de usuário e separação de token-security

--- security.py
+++ security.py
@@ -1,8 +1,11 @@
 from passlib.context import CryptContext
 from dotenv import load_dotenv
+from fastapi.security import  OAuth2PasswordBearer
 import os
+
 load_dotenv()
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
+OAuth2PasswordBearer= OAuth2PasswordBearer(tokenUrl="/auth/Login")
