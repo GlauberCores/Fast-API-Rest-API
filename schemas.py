f6359d2ee97856ecb770aaa1648a0580449dabd8
GlauberCores <glaubercores@gmail.com>
2026-03-17 00:44:27 -0300

    update

--- schemas.py
+++ schemas.py
@@ -20,3 +20,11 @@ class PedidoSchema(BaseModel):
    
     class Config:
         from_attributes = True
+
+
+class LoginSchema(BaseModel):
+    email: str
+    senha: str
+
+    class Config:
+        from_attributes = True
